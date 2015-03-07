ac81ee34fabd68c516e146a1144b77a5efc557d0
sungminchang <4sungmin@gmail.com>
2015-03-07 15:06:10 -0800

    Implement a live search feature on the links page.

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -3,6 +3,7 @@ angular.module('shortly.links', [])
 .controller('LinksController', function ($scope, $http, Links) {
   // Your code here
   $scope.data = {};
+  $scope.predicate = '-visits';
 
   $scope.getLinks = function() {
     Links.retrieveLinks()
@@ -16,4 +17,22 @@ angular.module('shortly.links', [])
   };
 
   $scope.getLinks();
+})
+.filter('searchFor', function(){
+    return function(arr, searchString){
+        if(!searchString){
+            return arr;
+        }
+        var result = [];
+        searchString = searchString.toLowerCase();
+        angular.forEach(arr, function(item){
+            if(item.url.toLowerCase().indexOf(searchString) !== -1){
+            result.push(item);
+        }
+        });
+        return result;
+
+        // Took code from
+        // http://code.ciphertrick.com/2015/02/07/live-search-using-custom-filter-in-angular-js/
+    };
 });
